d435612c19927066097fe56c2efe9a8accb6cfd5
Bubbles2016 <hsein.barada@gmail.com>
2017-06-20 15:19:37 +0300

    Project complete

--- resources/js/main.js
+++ resources/js/main.js
@@ -9,14 +9,15 @@ $(document).ready(function(){
     pauseOnHover: false
   });
 
+  /*Function to hide the navigation bar when the user scroll down the page*/
   var lastScrollTop = 0;
-  $(window).scroll(function(){
+  $(window).scroll(function(){  /*When the user scrolls down....*/
     var scrollTop = $(this).scrollTop();
-    if (scrollTop - lastScrollTop > 50){
+    if (scrollTop - lastScrollTop > 50){ /*If the user scrolls down more than 50px, we need to hide the nav bar by giving negative value to Top i.e: the nav bar will disappears*/
       var navHeight = $('.navbar').css('height');
       $('.navbar').animate({top: '-' + navHeight}, 150);
       lastScrollTop = scrollTop;
-    } else if (lastScrollTop - scrollTop > 50) {
+    } else if (lastScrollTop - scrollTop > 50) { /*If the user scrolls less than 50px then no need to hide the navbar*/
       $('.navbar').animate({top: '0px'}, 150);
       lastScrollTop = scrollTop;
     }
